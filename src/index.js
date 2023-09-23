7882ca1b6a2bb0bad1c146bee465f6f4750bdb7e
nhanhthat <84766117+nhanhthat@users.noreply.github.com>
2023-09-23 22:15:14 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,27 +24,20 @@ const Link = mongoose.model('Link', LinkSchema);
 let productIdCounter = 1;
 // Endpoint POST để ghi dữ liệu vào MongoDB
 app.post('/api/live_x', (req, res) => {
-  const newLink = new Link({ content: content });
-  let success = false; // Biến để theo dõi trạng thái thành công
 
-  newLink
-    .save()
+  const newLink = new Link({ content: content });
+  newLink.save()
     .then(() => {
-      success = true; // Đánh dấu thành công
-      return Link.countDocuments(); // Đếm số lượng Link đã lưu
-    })
-    .then((count) => {
-      if (success) {
-        const productIdCounter = count + 1; // Tăng biến đếm ID sản phẩm
-        res.status(200).json({ message: 'success', productIdCounter });
-      } else {
-        res.status(500).json({ message: 'Lỗi trong quá trình lưu dữ liệu' });
-      }
+      // Tăng biến đếm ID sản phẩm
+      productIdCounter++;
+      res.status(200).json({ message: 'success', productIdCounter});
     })
     .catch((err) => {
       console.error(err);
       res.status(500).json({ message: 'Lỗi trong quá trình lưu dữ liệu' });
     });
+
+  
 });
 
 app.listen(3000, () => {
